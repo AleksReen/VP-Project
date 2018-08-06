08ab9e73defb0e9172b51a3fc091d14572c0fc28
MINSK\Aliaksandr_Reyentovi <Aliaksandr_Reyentovich@epam.com>
2018-08-06 15:29:03 +0300

    completed Emp

---
+++ VPproject/Models/EmployerValidationModel.cs
@@ -0,0 +1,12 @@
+﻿namespace WpfDiplom.Models
+{
+    public class EmployerValidationModel
+    {
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string Patronymic { get; set; }
+        public string Title { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
