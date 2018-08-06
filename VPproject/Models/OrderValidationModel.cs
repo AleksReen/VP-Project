385626e5cd7ff6ad64c37e531468408f9de518af
MINSK\Aliaksandr_Reyentovi <Aliaksandr_Reyentovich@epam.com>
2018-08-06 11:53:16 +0300

    added validation for Orders

---
+++ VPproject/Models/OrderValidationModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WpfDiplom.Models
+{
+    public class OrderValidationModel
+    {
+        public int Quantity { get; set; }
+        public decimal Discount { get; set; }
+        public decimal Delivery { get; set; }
+    }
+}
